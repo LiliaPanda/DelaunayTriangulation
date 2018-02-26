f58f6461dbcc8c566784972be5b03cc0146df62a
LiliaPanda <liliapandav@gmail.com>
2018-02-26 13:52:15 +0200

    Added libs

--- src/com/lily/Main.java
+++ src/com/lily/Main.java
@@ -1,9 +1,13 @@
 package com.lily;
 
+import com.sun.j3d.utils.geometry.Sphere;
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Sphere sphere = new Sphere(12,12,10);
         System.out.println("OK");
     }
 }
